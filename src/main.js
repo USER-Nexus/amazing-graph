209bc62f8191e347b9883b97af67bbc7d447612f
USER-Nexus <henrique.p.lima0@gmail.com>
2021-06-09 08:17:09 -0300

    Seending header/navbar code

---
+++ src/main.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var x = document.getElementById("links");
+    var y = document.getElementById("linksIcon");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
